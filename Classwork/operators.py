8dd90995d39ba584113c2d31b6596910b4ee517c
varun keshetty <ramya.keshetty@gmail.com>
2025-07-10 14:51:56 +0530

    first program

---
+++ Classwork/operators.py
@@ -0,0 +1,25 @@
+#.PythonOperators.py
+
+#1. Arithmetic Operators
+
+a=100
+b=50
+
+print("addition (a+b):", a+b)
+print("subtraction (a-b):", a-b)
+print("multiplication (a*b):", a*b)
+print("division (a/b):", a/b)
+print("floor division (a//b):", a//b)
+print("modulus (a%b):",a%b)
+print("exponentiation (a**b):", a**b)
+
+#2. Comparison Operators
+c=80
+d=20
+
+print("equal to (c==d):",c==d)
+print("not equal to (c!=d):",c!=d)
+print("greater than (c>d):",c>d)
+print("less than (c<d):",c<d)
+print("greater than or equal to (c>=d):",c>=d)
+print("less than or equal to (c<=d):",c<=d)
